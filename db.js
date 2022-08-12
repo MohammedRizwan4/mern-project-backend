ecbac9e9981bb77c09ae9a3e0d2611bf73b887f7
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2022-08-13 04:14:00 +0530

    make it better

--- db.js
+++ db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb+srv://inotebook8:Rizwan0411@cluster0.zqrs4qo.mongodb.net/inotebook8?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI,() => {
+    mongoose.connect(mongoURI ,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    } ,() => {
         console.log("Connected to mongo successfully");
     })
 }
