3421b4c3bc82611dedff5ec910af02839e274139
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-08-02 00:21:58 +0530

    chore: Add CORS middleware to enable cross-origin requests

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const connectToMongo = require('./db');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+app.use(cors());
 
 const port = 5000
 
@@ -10,11 +11,6 @@ connectToMongo();
 
 //express server
 
-app.use(cors({
-  origin: ["https://mern-project-frontend-alpha.vercel.app/"],
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  credentials: true
-}));
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
